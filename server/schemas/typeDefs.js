324728baa1f49a77b77948bcbbb6ab7e66406ca0
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-12 16:17:17 -0700

    create the remaining query type definitions and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,14 @@ const { gql } = require('apollo-server-express');
 
 // create typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        thoughts: [Thought]
+        friends: [User]
+    }
     type Thought {
         _id: ID
         thoughtText: String
@@ -18,7 +26,10 @@ const typeDefs = gql`
         username: String
     }
     type Query {
+        users: [User]
+        user(username: String!): User
         thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
     }
 `;
 
