9925a258ac37e240ac23f148a83bb5ef41734b14
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-16 17:41:54 -0700

    enhance the profile page to improve user experience

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
+import Auth from '../utils/auth';
 
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -17,11 +18,25 @@ const Profile = () => {
     return <div>Loading...</div>
   }
 
+  // navigate to personal profile page if username is the logged-in user's
+  if(Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to='/profile' />
+  }
+
+  // display message if user is not logged in
+  if(!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
